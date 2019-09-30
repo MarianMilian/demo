4bfe8c0a47a1f2a8220482ce705baf0f6191c09a
MarianMilian <milanmarian@mail.ru>
2019-09-30 12:59:34 +0300

    init1

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
-//fjfjfgg
+//init 1
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
